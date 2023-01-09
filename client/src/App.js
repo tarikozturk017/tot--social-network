6755f68f073fcc883e9a50f8ae9e80ab65cea107
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-01-09 07:41:36 -0500

    theme/styling setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,28 @@ import {  BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from 'scenes/homepage';
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
+import { useMemo } from "react";
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
+// grabs the mode from our state (initial state)
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]) //setup the theme
+
   return (
     <div className="app">
       <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> 
         <Routes>
           <Route path='/' element={<LoginPage />} />
           <Route path='/home' element={<HomePage />} />
           <Route path='/profile/:userId' element={<ProfilePage />} />
         </Routes>
+      </ThemeProvider>
       </BrowserRouter>
     </div>
   );
